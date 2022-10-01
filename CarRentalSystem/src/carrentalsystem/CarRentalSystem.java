0b35dc0b6901bd697e2cafd72958ab69f4303c1a
Lee Yee Hau <66552240+yhau739@users.noreply.github.com>
2022-10-01 14:54:58 +0800

    Add files via upload

---
+++ CarRentalSystem/src/carrentalsystem/CarRentalSystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrentalsystem;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CarRentalSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame main = new MainFrame();
+        main.setVisible(true);
+    }
+    
+}
